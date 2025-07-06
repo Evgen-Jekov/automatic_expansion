65ce7968bf2becbbde08afba5e2d585a93adbf25
Evgen-Jekov <zekovevgen992@gmail.com>
2025-07-06 23:14:56 +0300

    del

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ python main.py
 """
 
 from grid_planner.core.network_builder import create_network
-from grid_planner.optimizer.graph_optimizer import optimize_network
 from pandapower.plotting import simple_plot
 
 net = create_network()
-simple_plot(net=net)
-o_net = optimize_network(net=net)
 simple_plot(net=net)
